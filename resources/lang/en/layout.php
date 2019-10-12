ea1f3b57fa71fa247b61bc23a6ab70942f58fcb4
Abd Alrahman Fani <faniabdo99@gmail.com>
2019-10-12 22:11:15 +0200

    Some SEO FIXES

--- resources/lang/en/layout.php
+++ resources/lang/en/layout.php
@@ -1,9 +1,9 @@
 <?php
     return [
         //Head
-        'HeadTitle' => ' Multiplayer Camel Racing Game 2020 – The Markad Racing',
+        'HeadTitle' => 'Markad Racing - Multiplayer Camel Racing Game 2020',
         'HeadDesc' => 'Get ready for the upcoming multiplayer camel racing game 2020 and it has multiple of Arabic tracks to play the camel racing. Sell and purchase the camels to get prepared for camel racing tournaments and don’t miss out the chance to get physical rewards.',
-        'HeadKeywords' => 'Multiplayer racing games, camel racing games, new camel games, al-watba track racing, Al-rehma racing game, Al-marmoom camel racing, best 2020 multiplayer games, online racing games, camel racing arena, Arabic camel racing games, new camel racing games, free camel racing games',
+        'HeadKeywords' => 'Markad Racing , Multiplayer racing games, camel racing games, new camel games, al-watba track racing, Al-rehma racing game, Al-marmoom camel racing, best 2020 multiplayer games, online racing games, camel racing arena, Arabic camel racing games, new camel racing games, free camel racing games',
         //Navbar
         'GameTitle' => 'MARKAD RACING',
         'Home' => 'Home',
