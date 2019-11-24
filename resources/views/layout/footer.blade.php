3e682b520ddd2609f2ab73881dc95c0c5007565e
Semicolon Group <faniabdo99@gmail.com>
2019-11-24 19:29:30 +0200

    Add Live Messages

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,3 +1,16 @@
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ddabcb743be710e1d1edde2/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <footer>
   <div class="container">
     <div dir="{{__('LandingPage.dir')}}" class="row {{__('LandingPage.textClass')}}">
