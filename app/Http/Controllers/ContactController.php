0c92066508e761e6074becf268d367c171d48777
faniabdo99 <faniabdo99@gmail.com>
2020-05-22 20:02:35 +0200

    Add Captcha to Contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -17,7 +17,9 @@ class ContactController extends Controller{
         $Validator = $r->validate([
           'user_name' => 'required|max:255|min:10',
           'user_contact_method' => 'required|max:255',
-          'message' => 'required|min:10'
+          'message' => 'required|min:10',
+          'captcha' => 'required',
+          'captcha_res' => 'required'
         ] , [
           'user_name.required' => 'Username is required',
           'user_name.max' => 'Username can\'t be more than 255 letters',
@@ -28,15 +30,21 @@ class ContactController extends Controller{
           'message.min' => 'Message can\'t be less than 10 letters'
         ]);
         //The Validator Passed , Insert The Data
-        $Message = new Message();
-        $Message->user_name = $r->user_name;
-        $Message->user_contact_method = $r->user_contact_method;
-        $Message->message = $r->message;
-        $Message->is_support_ticket = $isSupportTicket;
-        $Message->save();
-        //Mail The Admin About The New Message Here.
-        Mail::to('salem@markadracing.com')->send(new ContactNoticeMail($r->all()));
-        return back()->withSuccess('Your Message Has Been Recived , Thank You !');
+        if($r->captcha == $r->captcha_res){
+          //The Captcha Passed ...
+          $Message = new Message();
+          $Message->user_name = $r->user_name;
+          $Message->user_contact_method = $r->user_contact_method;
+          $Message->message = $r->message;
+          $Message->is_support_ticket = $isSupportTicket;
+          $Message->save();
+          //Mail The Admin About The New Message Here.
+          Mail::to('salem@markadracing.com')->send(new ContactNoticeMail($r->all()));
+          return back()->withSuccess('Your Message Has Been Recived , Thank You !');
+        }else{
+          return back()->with('captcha_error','Wrong Captcha Answer ! Try Again');
+        }
+
 
 
       }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,6 +10,12 @@
     <p>{{__('Pages.ContactUsP')}}</p>
     <div class="contact-form">
       <form action="{{route('ContactRouteProccess')}}" dir="{{__('LandingPage.dir')}}" method="post">
+        @if(session()->has('captcha_error'))
+          <span class="form-error-message">{{session('captcha_error')}}</span>
+        @endif
+        @if(session()->has('success'))
+          <span class="form-success-message">{{session('success')}}</span>
+        @endif
         @csrf
         <label class="{{__('LandingPage.textClass')}}">{{__('Pages.Name')}}</label>
         @error('user_name') <span class="form-error-message">{{$message}}</span> @enderror
@@ -17,17 +23,28 @@
         <label class="{{__('LandingPage.textClass')}}">{{__('Pages.ContactMethod')}}</label>
         @error('user_contact_method') <span class="form-error-message">{{$message}}</span> @enderror
         <input name="user_contact_method" type="text" placeholder="{{__('Pages.ContactMethodPlaceholder')}}" required maxlength="255" >
-        <label class="{{__('LandingPage.textClass')}}">{{__('Pages.Message')}}</label>
         @error('message') <span class="form-error-message">{{$message}}</span> @enderror
+        <input type="number" required name="captcha" class="contact-input" >
+        <input type="hidden" name="captcha_res" >
+        <label class="{{__('LandingPage.textClass')}}">{{__('Pages.Message')}}</label>
         <textarea name="message" placeholder="{{__('Pages.MessagePlaceholder')}}" required maxlength="1500"></textarea>
         <input type="submit" name="sendContactUs" value="{{__('Pages.SendAsContact')}}" >
         <input type="submit" name="sendSupport" value="{{__('Pages.SendAsTicket')}}" >
-        @if(session()->has('success'))
-          <span class="form-success-message">{{session('success')}}</span>
-        @endif
       </form>
     </div>
   </div>
 </body>
-{{-- @include('layout.footer') --}}
 @include('layout.scripts')
+<script>
+ $(document).ready(function(){
+   function randomInt(min, max) {
+     return min + Math.floor((max - min) * Math.random());
+   }
+   var RandomNumber1 =  randomInt(1,10);
+   var RandomNumber2 =  randomInt(1,10);
+   var resulte = RandomNumber2 + RandomNumber1;
+   console.log(resulte);
+   $('input[name="captcha"]').attr('placeholder' , 'ما نتيجة جمع ' + RandomNumber1 + ' و ' + RandomNumber2 + ' ؟' );
+   $('input[name="captcha_res"]').val(resulte);
+ });
+ </script>
